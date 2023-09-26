02d49dafcd671fbd9f015d2f4cd067225805a16b
derasoft004 <derasoft004@gmail.com>
2023-09-26 17:46:39 +0400

    переработка класса, оставлен только динамический массив который хранит в себе элементы и прописан каждый оператор, нуждается в распределении под .h и .cpp файлы и решении ошибки не влияющей на исполнение с *this (ошибка кучи)

--- CMakeProject_AISD/CMakeProject_AISD.cpp
+++ CMakeProject_AISD/CMakeProject_AISD.cpp
@@ -3,74 +3,130 @@
 
 using namespace std;
 
-class Element {
-	short width_ = 0, height_ = 0;
-	char weight_ = '0';
+
+template<typename T>
+class GrayscaleImage {
+	T* list_elements;
+	int width, height;
 public:
-	Element(short width, short height, char weight) {
-		width_ = width;
-		height_ = height;
-		weight_ = weight;
+
+	GrayscaleImage(int w, int h, bool feel): width(w), height(h) {
+		srand(time(0));
+		list_elements = new T[width * height];
+		random_device rd;
+		mt19937 gen(rd());
+		uniform_int_distribution<> dis(0, 10); // случайный Т от 0 до макс numeric_limits<T>::max()
+		for (int i = 0; i < width * height; i++) {
+			if (feel) list_elements[i] = dis(gen);
+			else list_elements[i] = 0;
+		}
+	}
+
+	T& operator()(int x, int y) { // оператор на запись
+		if (x >= 0 && x < width && y >= 0 && y < height) 
+			return list_elements[x * width + y];
+		else throw "Index out of range!";
+	}
+
+	const T& operator()(int x, int y) const { // чтиво
+		if (x >= 0 && x < width && y >= 0 && y < height)
+			return list_elements[x * width + y];
+		else throw "Index out of range!";
+	}
+
+	GrayscaleImage<T> operator*(T a) const {
+		GrayscaleImage<T> other(width, height, false);
+		for (int i = 0; i < width * height; i++) {
+			other.list_elements[i] = list_elements[i] * a;
+		}
+		return other;
+	}
+
+	GrayscaleImage<T> operator+(T a) {
+		GrayscaleImage<T> other(width, height, false);
+		for (int i = 0; i < width * height; i++) {
+			other.list_elements[i] = list_elements[i] + a;
+		}
+		return other;
+	}
+
+	/*
+	GrayscaleImage<T> operator*(T a) const {
 		
-	}
-	Element(short width, short height) {
-		width_ = width;
-		height_ = height;
-	}
-
-	Element(): width_(0), height_(0) {}
-
-	char get_weight() const {
-		return weight_;
-	}
-	void set_weight(char weight) {
-		weight_ = weight;
-	}
-	short get_height() const {
-		return height_;
-	}
-	short get_width() const {
-		return width_;
-	}
-};
-
-class Grayscale_Image {
-	Element** list_elemenst;
-	bool feel_;
-public:
-
-	Grayscale_Image(short widht, short height, bool feel) {
-		list_elemenst = new Element*[widht];
-		feel_ = feel;
-		if (feel) {
-			for (int i = 0; i < widht; i++) {
-				list_elemenst[i] = new Element[height];
-				for (int j = 0; j < height; j++) {
-					list_elemenst[i][j] = Element(i, j, '3');
-				}
-			}
+		for (int i = 0; i < width * height; i++) {
+			list_elements[i] *= a;
 		}
-		else {
-			for (int i = 0; i < widht; i++) {
-				list_elemenst[i] = new Element[height];
-				for (int j = 0; j < height; j++) {
-					list_elemenst[i][j] = Element(i, j, '0');
-				}
+		return *this;
+	}
+
+	GrayscaleImage<T> operator+(T a) {
+		for (int i = 0; i < width * height; i++) {
+			list_elements[i] += a;
+		}
+		return *this;
+	} - так выдает ошибку кучи _CrtlsValidHeapPointer
+	*/
+
+	GrayscaleImage<T> operator!(){
+		GrayscaleImage<T> other(width, height, false);
+		for (int i = 0; i < width * height; i++) {
+			other.list_elements[i] = ~list_elements[i] + 1;
+		}
+		return other;
+	}
+
+	float getFillCoefficient() const {
+		float sum = 0;
+		for (int i = 0; i < width * height; i++) {
+			sum += list_elements[i];
+		}
+		return sum / (width * height);
+	}
+
+	void print() const {
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				cout << list_elements[i * width + j] << " ";
 			}
+			cout << endl;
 		}
 	}
 
-	Element operator()(short x, short y) {
-		return list_elemenst[x][y];
+	~GrayscaleImage() {
+		delete[] list_elements;
 	}
-};
+};	
 
 
 int main()
 {
-	srand(time(0));
-	Grayscale_Image a(200, 300, true), b(200, 300, false);
-	cout << a(3, 5).get_weight() << ' ' << b(50, 200).get_weight() << endl;
-	cout << "Hello CMake." << endl;
+	setlocale(LC_ALL, "");
+	GrayscaleImage<int> image(3, 3, true);
+	image.print(); // принтанули
+
+	cout << "изменим 2-ой элемент на 11" << endl;
+	image(0, 1) = 11; // изменили
+	image.print();
+
+	GrayscaleImage<int> image_addition = image + 10;
+	cout << "прибавили 10" << endl;
+	image_addition.print(); // прибавили 10 
+
+	GrayscaleImage<int> inverted_image = !image_addition;
+	cout << "инвертируем" << endl;
+	inverted_image.print(); // инвертировали
+
+	GrayscaleImage<int> image_composition = inverted_image * 2;
+	cout << "умножим все на 2" << endl;
+	image_composition.print(); // умножили на 2 принтанули
+
+	cout << "пустая матрица" << endl;
+	GrayscaleImage<int> empty_image(3, 3, false);
+	empty_image.print();
+
+	float fillCoefficient = image.getFillCoefficient();
+	cout << "Fill coefficient: " << fillCoefficient << endl;
+
+	
 	return 0;
 }
